a1108341ebe866667f01ca4296738416df876599
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2020-04-21 11:50:11 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Nectrlline',
+    version='1',
+    description='Awesome library',
+    url='https://github.com/whatever/whatever',
+    author='amzon_0021',
+    author_email='riimuww@gmail.com',
+    license='MIT',
+    keywords='sample setuptools development',
+    packages=[
+        "line.py",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.7 :: amzon',
+    ],
+)
